073acb6db179a92f2d9b9880ff78140618fc6fe7
Hong-Long <whl1457139188@163.com>
2020-06-03 09:14:29 +0800

    Example1_7.m

--- Example1_7.m
+++ Example1_7.m
@@ -1,1 +1,11 @@
-clear, close all
+close all, clear all, clc
+% Define Vm = 170V
+Vm = 170;
+% Define period T = 2*pi
+T = 2*pi;
+% Divded 1024 points between 0 and 2*pi
+theta = linspace(0,T,1024);
+% Define the experssion about the V_t
+V_t = Vm*sin(theta);
+% Calculate the trapezoidal integral to calculate the output voltage
+Vo = 1 / T * trapz(theta,V_t)
